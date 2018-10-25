a6779e37ad61c379c64995d510ce3cd906c349e0
Anupknj <anupknjoshi@gmail.com>
2018-10-25 13:38:36 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { DataService } from "src/app/data.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+
+
+    constructor() {
+
+
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HousesComponent } from './houses/houses.component';
+import { NotfoundcomponentComponent } from './notfoundcomponent/notfoundcomponent.component';
+import { BooksviewComponent } from './booksview/booksview.component';
+import { CharactersviewComponent } from './charactersview/charactersview.component';
+import { HousesviewComponent } from './housesview/housesview.component';
+import { DataService } from './data.service';
+import { DatahttpService } from "src/app/datahttp.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    CharactersComponent,
+    HousesComponent,
+    NotfoundcomponentComponent ,
+    BooksviewComponent ,
+    CharactersviewComponent ,
+    HousesviewComponent  
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule,
+
+      RouterModule.forRoot([
+
+          { path: 'books', component: BooksComponent },
+          { path: 'booksview/:id',component :BooksviewComponent },
+          { path: 'characters', component: CharactersComponent },
+          { path : 'charactersview/:id',component: CharactersviewComponent },
+          { path: 'houses', component: HousesComponent },
+          { path :'housesview/:id', component:HousesviewComponent }, 
+          { path: '', redirectTo: '', pathMatch: 'full' },
+          { path :'',component:AppComponent }
+          
+          
+      ])
+
   ],
-  providers: [],
+  providers: [DatahttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
